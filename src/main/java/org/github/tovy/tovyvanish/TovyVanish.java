5638ca20af3f0da9e968dce03a5e6dbfccbc055e
Tovy <Jasper.brilleman@gmail.com>
2023-11-23 12:56:48 +0100

    Initial commit

---
+++ src/main/java/org/github/tovy/tovyvanish/TovyVanish.java
@@ -0,0 +1,17 @@
+package org.github.tovy.tovyvanish;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TovyVanish extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/github/tovy/tovyvanish/Vanishcmd.java
@@ -0,0 +1,2 @@
+package org.github.tovy.tovyvanish;public class Vanishcmd {
+}
